284765bcd4dc9460c9992bc709d0d3153d1e659a
behzoddilshodov <behzodpubg@gmail.com>
2022-07-23 15:43:45 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,356 @@
+
+let heroes = [
+  {
+		id: 54,
+		num: '054',
+		name: 'Psyduck',
+		img: 'http://www.serebii.net/pokemongo/pokemon/054.png',
+		type: ['Water'],
+		height: '0.79 m',
+		weight: '19.6 kg',
+		candy: 'Psyduck Candy',
+		candy_count: 50,
+		egg: '5 km',
+		spawn_chance: 2.54,
+		avg_spawns: 254,
+		spawn_time: '03:41',
+		multipliers: [2.27],
+		weaknesses: ['Electric', 'Grass'],
+		next_evolution: [
+			{
+				num: '055',
+				name: 'Golduck',
+			},
+		],
+	},
+
+  {
+		id: 57,
+		num: '057',
+		name: 'Primeape',
+		img: 'http://www.serebii.net/pokemongo/pokemon/057.png',
+		type: ['Fighting'],
+		height: '0.99 m',
+		weight: '32.0 kg',
+		candy: 'Mankey Candy',
+		egg: 'Not in Eggs',
+		spawn_chance: 0.031,
+		avg_spawns: 3.1,
+		spawn_time: '12:33',
+		multipliers: null,
+		weaknesses: ['Flying', 'Psychic', 'Fairy'],
+		prev_evolution: [
+			{
+				num: '056',
+				name: 'Mankey',
+			},
+		],
+	},
+
+  {
+		id: 61,
+		num: '061',
+		name: 'Poliwhirl',
+		img: 'http://www.serebii.net/pokemongo/pokemon/061.png',
+		type: ['Water'],
+		height: '0.99 m',
+		weight: '20.0 kg',
+		candy: 'Poliwag Candy',
+		candy_count: 100,
+		egg: 'Not in Eggs',
+		spawn_chance: 0.13,
+		avg_spawns: 13,
+		spawn_time: '09:14',
+		multipliers: [1.95],
+		weaknesses: ['Electric', 'Grass'],
+		prev_evolution: [
+			{
+				num: '060',
+				name: 'Poliwag',
+			},
+		],
+		next_evolution: [
+			{
+				num: '062',
+				name: 'Poliwrath',
+			},
+		],
+	},
+
+  {
+		id: 65,
+		num: '065',
+		name: 'Alakazam',
+		img: 'http://www.serebii.net/pokemongo/pokemon/065.png',
+		type: ['Psychic'],
+		height: '1.50 m',
+		weight: '48.0 kg',
+		candy: 'Abra Candy',
+		egg: 'Not in Eggs',
+		spawn_chance: 0.0073,
+		avg_spawns: 0.73,
+		spawn_time: '12:33',
+		multipliers: null,
+		weaknesses: ['Bug', 'Ghost', 'Dark'],
+		prev_evolution: [
+			{
+				num: '063',
+				name: 'Abra',
+			},
+			{
+				num: '064',
+				name: 'Kadabra',
+			},
+		],
+	},
+
+  {
+		id: 68,
+		num: '068',
+		name: 'Machamp',
+		img: 'http://www.serebii.net/pokemongo/pokemon/068.png',
+		type: ['Fighting'],
+		height: '1.60 m',
+		weight: '130.0 kg',
+		candy: 'Machop Candy',
+		egg: 'Not in Eggs',
+		spawn_chance: 0.0068,
+		avg_spawns: 0.68,
+		spawn_time: '02:55',
+		multipliers: null,
+		weaknesses: ['Flying', 'Psychic', 'Fairy'],
+		prev_evolution: [
+			{
+				num: '066',
+				name: 'Machop',
+			},
+			{
+				num: '067',
+				name: 'Machoke',
+			},
+		],
+	},
+
+  {
+		id: 71,
+		num: '071',
+		name: 'Victreebel',
+		img: 'http://www.serebii.net/pokemongo/pokemon/071.png',
+		type: ['Grass', 'Poison'],
+		height: '1.70 m',
+		weight: '15.5 kg',
+		candy: 'Bellsprout Candy',
+		egg: 'Not in Eggs',
+		spawn_chance: 0.0059,
+		avg_spawns: 0.59,
+		spawn_time: '12:19',
+		multipliers: null,
+		weaknesses: ['Fire', 'Ice', 'Flying', 'Psychic'],
+		prev_evolution: [
+			{
+				num: '069',
+				name: 'Bellsprout',
+			},
+			{
+				num: '070',
+				name: 'Weepinbell',
+			},
+		],
+	},
+
+  {
+		id: 74,
+		num: '074',
+		name: 'Geodude',
+		img: 'http://www.serebii.net/pokemongo/pokemon/074.png',
+		type: ['Rock', 'Ground'],
+		height: '0.41 m',
+		weight: '20.0 kg',
+		candy: 'Geodude Candy',
+		candy_count: 25,
+		egg: '2 km',
+		spawn_chance: 1.19,
+		avg_spawns: 119,
+		spawn_time: '12:40',
+		multipliers: [1.75, 1.76],
+		weaknesses: ['Water', 'Grass', 'Ice', 'Fighting', 'Ground', 'Steel'],
+		next_evolution: [
+			{
+				num: '075',
+				name: 'Graveler',
+			},
+			{
+				num: '076',
+				name: 'Golem',
+			},
+		],
+	},
+
+  {
+		id: 78,
+		num: '078',
+		name: 'Rapidash',
+		img: 'http://www.serebii.net/pokemongo/pokemon/078.png',
+		type: ['Fire'],
+		height: '1.70 m',
+		weight: '95.0 kg',
+		candy: 'Ponyta Candy',
+		egg: 'Not in Eggs',
+		spawn_chance: 0.011,
+		avg_spawns: 1.1,
+		spawn_time: '04:00',
+		multipliers: null,
+		weaknesses: ['Water', 'Ground', 'Rock'],
+		prev_evolution: [
+			{
+				num: '077',
+				name: 'Ponyta',
+			},
+		],
+	},
+
+  {
+		id: 82,
+		num: '082',
+		name: 'Magneton',
+		img: 'http://www.serebii.net/pokemongo/pokemon/082.png',
+		type: ['Electric'],
+		height: '0.99 m',
+		weight: '60.0 kg',
+		candy: 'Magnemite Candy',
+		egg: 'Not in Eggs',
+		spawn_chance: 0.023,
+		avg_spawns: 2.3,
+		spawn_time: '15:25',
+		multipliers: null,
+		weaknesses: ['Fire', 'Water', 'Ground'],
+		prev_evolution: [
+			{
+				num: '081',
+				name: 'Magnemite',
+			},
+		],
+	},
+
+  {
+		id: 90,
+		num: '090',
+		name: 'Shellder',
+		img: 'http://www.serebii.net/pokemongo/pokemon/090.png',
+		type: ['Water'],
+		height: '0.30 m',
+		weight: '4.0 kg',
+		candy: 'Shellder Candy',
+		candy_count: 50,
+		egg: '5 km',
+		spawn_chance: 0.52,
+		avg_spawns: 52,
+		spawn_time: '07:39',
+		multipliers: [2.65],
+		weaknesses: ['Electric', 'Grass'],
+		next_evolution: [
+			{
+				num: '091',
+				name: 'Cloyster',
+			},
+		],
+	},
+
+	{
+		id: 98,
+		num: '098',
+		name: 'Krabby',
+		img: 'http://www.serebii.net/pokemongo/pokemon/098.png',
+		type: ['Water'],
+		height: '0.41 m',
+		weight: '6.5 kg',
+		candy: 'Krabby Candy',
+		candy_count: 50,
+		egg: '5 km',
+		spawn_chance: 2.12,
+		avg_spawns: 212,
+		spawn_time: '03:33',
+		multipliers: [2.36, 2.4],
+		weaknesses: ['Electric', 'Grass'],
+		next_evolution: [
+			{
+				num: '099',
+				name: 'Kingler',
+			},
+		],
+	},
+
+	{
+		id: 109,
+		num: '109',
+		name: 'Koffing',
+		img: 'http://www.serebii.net/pokemongo/pokemon/109.png',
+		type: ['Poison'],
+		height: '0.61 m',
+		weight: '1.0 kg',
+		candy: 'Koffing Candy',
+		candy_count: 50,
+		egg: '5 km',
+		spawn_chance: 0.2,
+		avg_spawns: 20,
+		spawn_time: '08:16',
+		multipliers: [1.11],
+		weaknesses: ['Ground', 'Psychic'],
+		next_evolution: [
+			{
+				num: '110',
+				name: 'Weezing',
+			},
+		],
+	},
+]
+
+
+let elList = document.querySelector(".list")
+let elLike = document.querySelector("like2");
+elList.classList.add( "list-unstyled", "d-flex", "row", "justify-content-between", "mt-5" )
+function dragon(dragon) {
+  for (let i = 0; i < heroes.length; i++) {
+    let li = document.createElement("li");
+		elList.appendChild(li);
+		li.classList.add("item", "col-4")
+
+		newDiv = document.createElement("div");
+		li.appendChild(newDiv);
+		newDiv.classList.add("card-body", "d-flex", "flex-column", "align-items-center")
+
+    newImg = document.createElement("img")
+    newDiv.appendChild(newImg);
+    newImg.src = heroes[i]["img"];
+		newImg.classList.add("card-img-top", "pt-3")
+
+		newTitle = document.createElement("h5");
+		newDiv.appendChild(newTitle);
+		newTitle.textContent = heroes[i]["name"];
+		newTitle.classList.add("card-title");
+
+		newP = document.createElement("p");
+		newDiv.appendChild(newP);
+		newP.textContent = heroes[i]["type"];
+		newP.classList.add("fs-4", "mt-4");
+
+		heightP = document.createElement("p");
+		newDiv.appendChild(heightP);
+		heightP.textContent = heroes[i]["height"]
+		heightP.classList.add("fs-4");
+
+		weightP = document.createElement("p");
+		newDiv.appendChild(weightP);
+		weightP.textContent = heroes[i]["weight"]
+		weightP.classList.add("fs-4");
+
+
+
+    
+  }
+}
+
+dragon()
+
+
+// console.log(elList);
